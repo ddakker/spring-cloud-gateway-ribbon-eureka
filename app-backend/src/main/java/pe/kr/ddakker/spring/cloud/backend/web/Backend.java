10b29ea89c18420de2807ce9a715ce5284ed0495
ddakker <ddakker@opennaru.com>
2020-05-12 15:26:03 +0900

    idea http request example

---
+++ app-backend/src/main/java/pe/kr/ddakker/spring/cloud/backend/web/Backend.java
@@ -0,0 +1,11 @@
+package pe.kr.ddakker.spring.cloud.backend.web;
+
+public class Backend {
+    public int seq;
+    public String name;
+
+    public Backend(int seq, String name) {
+        this.seq = seq;
+        this.name = name;
+    }
+}
--- app-backend/src/main/java/pe/kr/ddakker/spring/cloud/backend/web/BackendApiController.java
+++ app-backend/src/main/java/pe/kr/ddakker/spring/cloud/backend/web/BackendApiController.java
@@ -2,9 +2,15 @@ package pe.kr.ddakker.spring.cloud.backend.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 public class BackendApiController {
@@ -21,8 +27,17 @@ public class BackendApiController {
 	}
 
 	@GetMapping("/goods")
-	public String goods() {
-		System.out.println("backend api goods");
+	public String goods(HttpServletRequest request) {
+		String t = request.getParameter("t");
+		System.out.println("backend api goods t: " + t);
+		Enumeration<String> parameterNames = request.getParameterNames();
+		while (parameterNames.hasMoreElements()) {
+			String key =  parameterNames.nextElement();
+			System.out.println("param: " + key + "=" + request.getParameter(key));
+		}
+		System.out.println("header h: " + request.getHeader("h"));
+		System.out.println("header khan.tid: " + request.getHeader("khan.tid"));
+
 		String serverPort = environment.getProperty("local.server.port");
 		
 		return "goods - appName: " + appName + ", serverPort: " + serverPort;
@@ -34,5 +49,10 @@ public class BackendApiController {
 		String serverPort = environment.getProperty("local.server.port");
 
 		return "goodsOptions - appName: " + appName + ", serverPort: " + serverPort;
+	}
+
+	@GetMapping("/api/test1")
+	public Backend api_test1() {
+		return new Backend(1, "ddakker");
 	}
 }
